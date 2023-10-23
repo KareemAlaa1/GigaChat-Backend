c35bcf8c6186fe0ff8f7171a161f3d62fdf1db92
Malek-2002 <malek.200290mh@gmail.com>
2023-10-23 19:56:12 +0300

    make server connect to database and lestin to env port

---
+++ server.js
@@ -0,0 +1,17 @@
+
+require('dotenv').config();
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+
+
+
+mongoose.connect('mongodb://127.0.0.1/gigachat')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...'));
+
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Listening on port ${port}...`));
