6b0968f81fe8afba66cef7fcc604dfadaf7858da
A-OMAR-1234 <113605371+A-OMAR-1234@users.noreply.github.com>
2023-10-27 23:49:28 +0200

    user Routes , wrong Routes , globalErrorHandling

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+// MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+app.use(express.json());
+// app.use(express.static(`${__dirname}/public`));// for static data in public
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+
+//  ROUTES
+app.use('/api/v1/users', userRouter);
+
+// Handling  Wrong Route Req. 
+app.all('*', (req, res, next) => {
+  //create ourError obj and send it
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler); // the final middleWare for express
+
+module.exports = app;
+
 
