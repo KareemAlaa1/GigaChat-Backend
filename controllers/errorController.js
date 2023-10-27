6cf2abe6080bda62a8d2e17ef33c000f63956197
A-OMAR-1234 <113605371+A-OMAR-1234@users.noreply.github.com>
2023-10-27 23:43:18 +0200

    Global Error Handler {dev only}

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,1 +1,23 @@
+const sendErrorDev = (err, res) => {
+  // we will send all possible info about the errer
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
 
+
+// GlobalErrorHandler
+module.exports = (err, req, res, next) => {
+  // usual lines we talk about
+  err.statusCode = err.statusCode || 500;// internal program error
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    //TODO latter
+  }
+};
