3f1e6cf31978d91658b892ac39a18b003cc5f581
Sara Bisheer <sara.nousir@gmail.com>
2023-11-04 03:21:36 +0200

    Notification Model File Name Updated

---
+++ models/notification_model.js
@@ -0,0 +1,42 @@
+const { mongoose, Schema } = require('mongoose');
+const notificationSchema = new Schema({
+  description: {
+    type: String,
+  },
+  type: {
+    type: String,
+    required: [true, 'A notification must have a type'],
+    enum: {
+      values: ['like', 'reply', 'mention', 'retweet', 'follow'],
+      message:
+        'notification type is either: like, reply, mention, retweet, follow',
+    },
+  },
+  destination: {
+    type: String,
+    required: [true, 'A notification must have a destination'],
+  },
+  seen: {
+    type: Boolean,
+    required: true,
+  },
+  creation_time: {
+    type: Date,
+    default: Date.now(),
+    required: true,
+  },
+  notifier: {
+    type: Schema.Types.ObjectId,
+    required: [true, 'A notification must have a notifier'],
+    ref: 'User',
+  },
+  notified: {
+    type: Schema.Types.ObjectId,
+    required: [true, 'A notification must have a notified user'],
+    ref: 'User',
+  },
+});
+
+const Notification = mongoose.model('Notification', notificationSchema);
+
+module.exports = Notification;
