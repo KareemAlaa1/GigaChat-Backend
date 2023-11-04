65437685d616d70ee69c04c820b089b200a0ff4a
Sara Bisheer <sara.nousir@gmail.com>
2023-11-04 03:20:35 +0200

    Message Model Creation

---
+++ models/message_model.js
@@ -0,0 +1,37 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var messageSchema = new Schema({
+  discription: {
+    type: String,
+    required: true,
+  },
+  media: {
+    photo: {
+      type: String,
+    },
+    vedio: {
+      type: String,
+    },
+  },
+  sender: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  seen: {
+    type: Boolean,
+    required: true,
+  },
+  sendTime: {
+    type: Date,
+    required: true,
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Message = mongoose.model('Message', messageSchema);
+
+module.exports = Message;
