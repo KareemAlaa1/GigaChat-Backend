8bad34b11d8139f1e3b05ccc9954283b5f9082ae
Malek-2002 <malek.200290mh@gmail.com>
2023-11-06 22:53:32 +0200

    add "/api/user/profile" to the user route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,1 +1,14 @@
 
+const express = require('express');
+const userRouter = express.Router();
+const UserController = require('../controllers/userController');
+
+
+userRouter.post('/profile', UserController.update_profile);
+
+
+
+module.exports = userRouter; 
+
+
+
