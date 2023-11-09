44148198b2d0becd8181319251a0483182099b69
Ahmed Omer <ahmedomer13218@gmail.com>
2023-11-09 20:51:40 +0200

    Authorization to protected route

--- controllers/authController.js
+++ controllers/authController.js
@@ -53,3 +53,52 @@ exports.login = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1) Getting token and check of it's there
+  // in postman in headers we set key to Authorization and value to "Barer tokenValue"
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+    // after splite take the second element
+  }
+
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access.', 401),
+    );
+  }
+
+  // 2) Verification token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET); // this function {jwt.verify(token,secret,callback fun)} takes 3 argument -> the callback once the verification happen but here we promisifying it and await it
+  // docoded -> is the docoded payload from jwta
+  //console.log(decode);//take alook at the shape of it
+
+  // 3) Check if user still exists
+  // if the user deleted we no more need to send data
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(
+      new AppError(
+        'The user belonging to this token does no longer exist.',
+        401,
+      ),
+    );
+  }
+
+  // 4) Check if user changed password after the token was issued
+  // users usually change their passwork if somone steal his token
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    //decoded.iat -> issued at
+    return next(
+      new AppError('User recently changed password! Please log in again.', 401),
+    );
+  }
+
+  // GRANT ACCESS TO PROTECTED ROUTE
+  req.user = currentUser; // will be usefull later
+  next();
+});
