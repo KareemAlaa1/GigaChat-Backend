5dea04c4749f306b1d26aaaa84b5f3ce984fc64a
Sara Bisheer <sara.nousir@gmail.com>
2023-11-09 12:46:36 +0200

    Get Hashtag Tweets Route

--- routes/hashtag_router.js
+++ routes/hashtag_router.js
@@ -3,5 +3,6 @@ const HashtagController = require('../controllers/hashtag_controller');
 const router = new express.Router();
 
 router.get('/trends/all', HashtagController.getAllHashtages);
+router.get('/trends/:trend', HashtagController.getHastagTweets);
 
 module.exports = router;
