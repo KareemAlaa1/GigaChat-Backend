016457d4ac7db74cdfefc2b798e1c659520f9dc5
Ahmed Omer <ahmedomer13218@gmail.com>
2023-11-09 20:47:35 +0200

    signUp endPoint

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,1 +1,29 @@
-// hello 
+const jwt = require('jsonwebtoken');
+const { promisify } = require('util'); //util.promisify
+const AppError = require('../utils/appError');
+const User = require('../models/user_model');
+const catchAsync = require('../utils/catchAsync');
+
+const signToken = (id) =>
+  jwt.sign({ id: id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
+exports.signUp = catchAsync(async (req, res, next) => {
+  const newUser = await User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+    phone: req.body.phone,
+    joinedAt: Date.now(),
+  });
+  const token = signToken(newUser._id);
+  res.status(201).json({
+    status: 'success',
+    token,
+    data: {
+      user: newUser,
+    },
+  });
+});
