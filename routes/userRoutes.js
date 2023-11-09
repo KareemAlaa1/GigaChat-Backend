1f93330290cccf61025031ee36ab9d9e272e545a
Ahmed Omer <ahmedomer13218@gmail.com>
2023-11-09 20:55:49 +0200

    adding singUp , login , updateMe and deleteMe routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
+const router = express.Router();
+
+router.all('/signup', authController.signUp);
+router.post('/login', authController.login);
+router.patch('/updateMe', authController.protect, userController.updateMe);
+router.delete('/deleteMe', authController.protect, userController.deleteMe);
+
+module.exports = router;
