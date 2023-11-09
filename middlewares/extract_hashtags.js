a43eaa4355efb6195b8b4fea1fe1280b7d852b3f
Sara Bisheer <sara.nousir@gmail.com>
2023-11-09 10:09:31 +0200

    "Exract Hastags Middleware"

---
+++ middlewares/extract_hashtags.js
@@ -0,0 +1,35 @@
+const catchAsync = require('../utils/catchAsync');
+const Hashtag = require('../models/hashtag_model');
+
+// extract the hashtags in the tweet description
+const extractHashtags = async (tweet) => {
+  const tweetId = tweet._id;
+  const tweetDescription = tweet.description;
+  const words = tweetDescription.split(' ');
+  const hashtagWords = words.filter((word) => /^#/.test(word));
+  await hashtagWords.forEach(async (hashtagWord) => {
+    const hashtag = await Hashtag.findOne({ title: hashtagWord });
+    console.log(hashtag);
+    // Check for this hashtage if exists in database
+    // if exists update its data and save
+    if (hashtag) {
+      hashtag.count++;
+      hashtag.tweet_list.push(tweetId);
+      hashtag.save();
+    } else {
+      // not exist so create it and save in database
+      new Hashtag({
+        title: hashtagWord,
+        count: 1,
+        tweet_list: [tweetId],
+      }).save();
+    }
+  });
+};
+
+// const extractHashtagsTest = async () => {
+//   const tweet = await Tweet.findById('654c193b688f342c88a547e8');
+//     extractHashtags(tweet);
+// };
+
+// extractHashtagsTest();
