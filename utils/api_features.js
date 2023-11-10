625139f41d5d9967bd326bc04e816e72bd0dcd02
Sara Bisheer <sara.nousir@gmail.com>
2023-11-10 14:30:51 +0200

    Refactor api_features file

--- utils/api_features.js
+++ utils/api_features.js
@@ -12,4 +12,31 @@ class APIFeatures {
     }
   }
 }
-module.exports = APIFeatures;
+
+selectNeededInfoForUser = async (tweet, req) => {
+  req.query.type = 'array';
+  req.query.fields =
+    '_id,username,nickname,bio,profileImage,followingUsers,followersUsers';
+  const apiFeatures = new APIFeatures(
+    [tweet.tweetOwner],
+    req.query,
+  ).limitFields();
+  tweet.tweetOwner = await apiFeatures.query;
+};
+
+selectNeededInfoForTweets = async (tweets, req) => {
+  req.query.type = 'array';
+  req.query.fields =
+    '_id,description,media,type,referredTweetId,likersList,repliesList,retweetList,views,createdAt,tweetOwner,';
+  const apiFeatures = new APIFeatures(tweets, req.query)
+    .sort()
+    .paginate()
+    .limitFields();
+  return await apiFeatures.query;
+};
+
+module.exports = {
+  APIFeatures,
+  selectNeededInfoForUser,
+  selectNeededInfoForTweets,
+};
