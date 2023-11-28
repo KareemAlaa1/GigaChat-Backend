ef95c3b37924819cec24dbda729e9a3b8b880023
amr-salahuddin <ar.slhdn@gmail.com>
2023-11-28 22:34:12 +0200

    Auth Unit Testing /api/user/confirmEmail

--- __test__/auth.test.js
+++ __test__/auth.test.js
@@ -39,4 +39,72 @@ describe('auth', () => {
         await deleteUser(testUser);
         await mongoose.disconnect();
     });
+
+    describe('POST /api/user/confirmEmail', () => {
+
+        it('responds with 201 and a token when email confirmation is successful', async () => {
+            let newUser = {
+                email:"test@test.com",
+                nickname:"testtest",
+                birthDate:"2000-01-01",
+                confirmEmailCode:"548a2bb0611b27e503ce5cb8b2123bf341a3ae18c3e7ba98620196572a3bab48",
+                confirmEmailExpires: "2024-01-01"
+            }
+            let newuseruser = await createUser(newUser);
+            console.log(newuseruser,'sadasdasd')
+            const response = await request(app)
+                .post('/api/user/confirmEmail')
+                .send({
+                    confirmEmailCode: '219589745',
+                    email: "test@test.com",
+                });
+            console.log( response.body);
+            expect(response.status).toBe(201);
+            expect(response.body.status).toBe('success');
+            expect(response.body.token).toBeDefined();
+            expect(response.body.data.suggestedUsername).toBeDefined();
+            expect(response.body.data.message).toBe('Confirm done successfully');
+            await User.deleteOne({email:"test@test.com"})
+
+        });
+
+        it('responds with 400 when email or confirmEmailCode is missing', async () => {
+            const response = await request(app)
+                .post('/api/user/confirmEmail')
+                .send({
+                    email: testUserData.email,
+                });
+
+            expect(response.status).toBe(400);
+            expect(response.body.message).toBe('email and confirmEmailCode required');
+        });
+
+        it('responds with 400 when email format is invalid', async () => {
+            const response = await request(app)
+                .post('/api/user/confirmEmail')
+                .send({
+                    confirmEmailCode: 'valid-code',
+                    email: 'invalid-email',
+                });
+            console.log(response.body);
+            expect(response.status).toBe(400);
+            expect(response.body.error).toBe('Invalid email format');
+        });
+
+        it('responds with 404 when there is no inactive user with the provided email', async () => {
+            const response = await request(app)
+                .post('/api/user/confirmEmail')
+                .send({
+                    confirmEmailCode: 'valid-code',
+                    email: 'nonexistent@example.com',
+                });
+
+            expect(response.status).toBe(404);
+            expect(response.body.message).toBe(
+                'There is no inactive user with  this email address.'
+            );
+        });
+
+        // Add more test cases as needed to cover different scenarios
+    });
 });
