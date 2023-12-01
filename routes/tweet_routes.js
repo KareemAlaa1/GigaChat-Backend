cf8b4243692240775660c685e9c1dc5998c09117
Malek-2002 <malek.200290mh@gmail.com>
2023-12-01 14:30:46 +0200

    make tweet route cleaner

--- routes/tweet_routes.js
+++ routes/tweet_routes.js
@@ -5,28 +5,14 @@ const authController = require('../controllers/auth_controller');
 const tweetRouter = express.Router();
 
 tweetRouter.post('/', authController.protect, tweetController.addTweet);
+
 tweetRouter.get('/:tweetId', authController.protect, tweetController.getTweet);
-tweetRouter.delete(
-  '/:tweetId',
-  authController.protect,
-  tweetController.deleteTweet,
-);
-tweetRouter.get(
-  '/likers/:tweetId',
-  authController.protect,
-  tweetController.getTweetLikers,
-);
 
-tweetRouter.patch(
-  '/retweet/:tweetId',
-  authController.protect,
-  tweetController.retweetTweet,
-);
+tweetRouter.delete('/:tweetId', authController.protect,tweetController.deleteTweet);
+tweetRouter.get('/likers/:tweetId', authController.protect,tweetController.getTweetLikers,);
 
-tweetRouter.get(
-  '/replies/:tweetId',
-  authController.protect,
-  tweetController.getTweetReplies,
-);
+tweetRouter.patch('/retweet/:tweetId', authController.protect,tweetController.retweetTweet,);
+
+tweetRouter.get('/replies/:tweetId', authController.protect, tweetController.getTweetReplies);
 
 module.exports = tweetRouter;
