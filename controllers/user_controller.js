eff8937ba5d9767d954ba80d94a0b53ca8aae745
Malek-2002 <malek.200290mh@gmail.com>
2023-12-02 01:02:43 +0200

    test query

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -198,7 +198,7 @@ exports.getCurrUserProfile = async (req, res) => {
 exports.updateProfile = async (req, res) => {
   try {
     // get the sent data from the request body
-    const { bio, location, website, nickname, birth_date } = req.body;
+    const { bio, location, website, nickname, birth_date } = req.query;
 
     if (!bio && !location && !website && !nickname && !birth_date) {
       return res.status(400).send({ error: 'Bad Request' });
