de0401de60f533227ac5d0e775a12b97a923405a
Kareem Alaa Abdullah <96543783+KareemAlaa1@users.noreply.github.com>
2023-12-05 18:31:57 +0200

    Update google_router.js

--- routes/google_router.js
+++ routes/google_router.js
@@ -1,43 +1,44 @@
-const router = require("express").Router();
-const passport = require("passport");
+const router = require('express').Router();
+const passport = require('passport');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config/dev.env' });
 
-const CLIENT_URL = "http://localhost:3001";
+const CLIENT_URL = process.env.FRONTEND_URL;
 
-router.get("/login/success", (req, res) => {
-    if (req.user) {
-        res.status(200).json({
-            success: true,
-            message: "successful",
-            user: req.user,
-            // cookies: req.cookies
-        });
-    }
-});
-
-router.get("/login/failed", (req, res) => {
-    res.status(401).json({
-        success: false,
-        message: "failure",
+router.get('/login/success', (req, res) => {
+  if (req.user) {
+    res.status(200).json({
+      success: true,
+      message: 'successful',
+      user: req.user,
+      // cookies: req.cookies
     });
+  }
 });
 
-router.get("/logout", (req, res) => {
-    req.logout();
-    res.redirect(CLIENT_URL);
+router.get('/login/failed', (req, res) => {
+  res.status(401).json({
+    success: false,
+    message: 'failure',
+  });
 });
 
-router.get("/google", passport.authenticate("google", { scope: ["profile","email"] }));
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect(CLIENT_URL);
+});
 
 router.get(
-    "/google/callback",
-    passport.authenticate("google", {
-        successRedirect: CLIENT_URL,
-        failureRedirect: "/login/failed",
-    })
+  '/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] }),
 );
 
+router.get(
+  '/google/callback',
+  passport.authenticate('google', {
+    successRedirect: CLIENT_URL,
+    failureRedirect: '/login/failed',
+  }),
+);
 
-
-
-
-module.exports = router
+module.exports = router;
