e906ad298bc6882706cc61f82b5c7b349ea2e0c3
Sara Bisheer <sara.nousir@gmail.com>
2023-12-09 19:55:33 +0200

    hashtag search added

--- routes/hashtag_router.js
+++ routes/hashtag_router.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const HashtagController = require('../controllers/hashtag_controller');
+const hashtagController = require('../controllers/hashtag_controller');
+const searchController = require('../controllers/search_controller');
 const { protect } = require('../controllers/auth_controller');
 const router = new express.Router();
 
-router.get('/all', protect, HashtagController.getAllHashtages);
-router.get('/:trend', protect, HashtagController.getHastagTweets);
-
+router.get('/all', protect, hashtagController.getAllHashtages);
+router.get('/search', protect, searchController.search);
+router.get('/:trend', protect, hashtagController.getHastagTweets);
 module.exports = router;
