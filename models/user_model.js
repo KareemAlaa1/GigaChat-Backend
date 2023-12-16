3f2daa6e51055a2c40b928c577bab555e0e19348
Amr Salahuddin <ar.slhdn@gmail.com>
2023-12-16 16:49:23 +0200

    follow/unfollow show error message

--- models/user_model.js
+++ models/user_model.js
@@ -91,8 +91,8 @@ const userSchema = new mongoose.Schema(
         type: {
           type: String,
           enum: {
-            values: ['tweet', 'quote', 'reply'],
-            message: 'type must be tweet or quote or reply',
+            values: ['tweet', 'retweet', 'quote', 'reply'],
+            message: 'type must be tweet or retweet or quote or reply',
           },
         },
       },
