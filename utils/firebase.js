e643e755f0127661a76e0299d0c625faf8a8a6d0
Kareem Alaa Abdullah <96543783+KareemAlaa1@users.noreply.github.com>
2023-12-27 07:59:41 +0200

    Update firebase.js

--- utils/firebase.js
+++ utils/firebase.js
@@ -2,21 +2,24 @@ const admin = require('firebase-admin');
 const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
 
+
+
 // Initialize Firebase Admin SDK
 admin.initializeApp({
   credential: admin.credential.cert({
     "type": "service_account",
-    "project_id": "gigachat-img",
-    "private_key_id": "3e34fc3ce136a7c81b02b3390b0dc3166bfe1e14",
-    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCguXp+h2+8MPz6\n9XIgp7lKq1FwtiblVRx2C83L5Ma+nFpqCwacR04RWKq0O3QHJifVi9C7tNRX4Jbi\nUKZ/RA9iyQSMASLzaoNNqv91W+2gJPMqnex3XbVpWsSiwnRAv/Ow69mr/f8RKpQH\nVk3inK9YzOSRiVp/8vQeQkC/GWAx0py39QPlxG0YXtQemSfF1URecwW85+tRNKZN\n9wVNbcJiJY2qy8ECahO98znIudvpPZ7cryAXllot9JCp37A1NO6lzr/Ukcybvm/0\nU0W1KDi43gx/Ruhlh4nFbSUAw4EL4vLGeX6iGTpKkaSt6NpHLaTN/vyH8Lae9kff\nN6bUWa4nAgMBAAECggEAAxEax2fGFl6tT8NA0w2k0z3gMc4+E3jxSJwd6Fw6Fajt\nPwULgay/yFzbSTLuvtjwQGheC/quiaH3+oHS+32lTnbpepWpQLxGwWhnRgvduWzL\n1PFAHJFzbGKx6Jrc/X34WzxnJSGhpwyhjvG2BVGQ7B8XzHq+5s8osvFljOF3OBkU\ncW1ADU/4gOyIvBczSl09mFvPxGAT4ukdjuihZOwR+fyAS0dZFZkFJswaXajr2PUp\nxV8NGwNeixagXY+AZdZNtnhwvXvUT2QH+segSqXszxCnac8kpB+6ZZwDnBc81DWl\nvEr2kst04GETohviucZfhmxcQarHReNk9ymHwnBLsQKBgQDSGHKa+fDYtTmGrAAQ\nNk1vzuIJTSmSR8uuWj+aYem92TNivHzez1EKsO2EHKuneUcdFB+yVoAQZOokU8s1\nuUHLAlsVozBjVlIA4lcQPRA8RCuf2hYsnrOSSNXV2Y6cv3E6zEcEZ2kpt3MDVNov\nl09uPrvzpAFag2Mfne/gXiUVaQKBgQDD13+ImGBc+uPEMML1/6nrLk/j9ZIYb+w4\nm4YlcB/Jd9teFhYS/b5UfccIoUELql9dz18qL3Uh6Kifg/kTJDLAUgMvUkgKJCkn\nPpibDHeXdK4ie6BvThuFFG8hboH1cSp/xWnREWDKaSwth4TIHkvFc+v9UEK6z/oF\nn1FzqoNlDwKBgQC5R54qVim0s32b6qvToFACPmzQzx/ORUq25BgbSiYcqyPJ+3gT\n9rj9uLJ+Q9qNw0KXvluFpwshah2jE9Kt+kavhdBV8KiHsmR3KySkyXU78NKYEnhj\nuUGbzGfoRAik66ArvmMS2y++/m2LjpUsS0BddS3D2+rlq+EKzPny4JPLoQKBgQC0\nCjPzhmnocLndjhpdftleHOhTIFpb3uDiNZAAcPC4DVj9SP6oHN/R2wOUGnKq38jm\nfBL3vENJlhMW+oOLplaF2Z+V7GXP9OhkbLZsq+xxr1G6375hNQQ1ttBngqk01AGH\nDxy1l+1Mh52WiaR3OOrdrjFAYn3GMhae/pUp6ZL73wKBgHYwkCb3w7JKbfLJHkSU\n/WkDrRtg8sppliqRNiUW6yCJjjr4tLepYkAUE+x+yOafGNfJBWXF/DS9qSFeBrxo\nKCPo4H/kNS2xykTR3kRhPhjq9JrCGOCXnFCxypjDhsXnr4h2BBJzDPRx2K39Rg3K\n45V0RbKVcpAdXCx8joxGF1oa\n-----END PRIVATE KEY-----\n",
-    "client_email": "firebase-adminsdk-5avio@gigachat-img.iam.gserviceaccount.com",
-    "client_id": "110836219980501782626",
+    "project_id": "tempgigachat",
+    "private_key_id": "f61ce8ab6117a5438bec04c46ed03ffc0085576b",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCqYY50+FZDiwVv\nSu+4w9RkQx+eT/0Y3FFaiOwrAOyBCWtLxskOzWmm4xUaHM3pAWMECcESZUYivZYJ\nJCUcosMQ0uuQOqEV5Pfo/bnuVQZcledqO1XzVjyE7kbKXLqYHascNlJk8gBMeFup\n8n4iyuk98x5PISXNn6vXF3nb0q3000Q62G2iiAhq+ULKrsYoazdPWqZQOhrnedwS\nbAGNgO0D2qPoJvYY6aztSOg4WPLFuqyj0pHO1/18d8W0o/5Ma51m/7B+f1RyG1Yl\nO3GiExL7LkEE9xkf60XQIvkyuALnW68rPnt4alr4i3Jg0nstVlnTB0sHXIRHIZfW\nf/r2IObFAgMBAAECggEAB1NT9mwB+3vz8x3H8kv7Kwdmyd37DhFU7x1ReCTdRkwD\nILouhVQoHXyIePuxj7nXScJomnOfwRkhJQn4jYNi+ytyZNxcdi5GyW31S029DQiF\n71+XObPkvLto8ETt4eHurDMatkvgXulS2Zr7OOA8CIWWMUVF1JIvp3sTKnQjthG4\nOWyRGqazABL79z06tT++a7xVfhxJZKcNBhK/e+VRNtbTX9zXp8ShOeaZdsxLt74k\n3xU+NH4rPqO0hCn17pyPEOPuhyJeRxZi+QOkRuRyjniexOKFoIxolUJoCReVIvTW\nFOkcbNSvrH7zGfr1nhS7m3GrjjZh3NZt/2mjbiFqIQKBgQDqzmVPFadwCf6N2xcJ\nuWHZPkyn0mDR6Zjsr5R1XQrp6Gt8sVMvhNXTxv2Qqbtmna4RqeAnSQCFB1hqUglj\nngOY4On05zEVtqxmmna64CHHdoNesqGvJMTHNJ1IT5y17R4iMCJU6FyClecfpgSI\nRHA8EATnf4hfm2ZCIlqTBUpbZQKBgQC5woGP9cMIGkwhPBKqGy8Z7dN8qxFotG3s\nIHjfvOocn18Nh/v/wpdEYCkaVOxQtm2I5hXSjOTq+NFkKhKuEl4k3zYm4cqhW1Gs\n+pMsOhj08Wy946YRO/5B8rgQjhpwhSJEzHwskHPq8rkS+vDfcN+aaw++gef8235j\nO3tA2YaX4QKBgAvAaLGck9S3q1XOMUj7TjphM9TyYoYe+thdsUBud61dRIPpHBC0\nxT7kJqym0gpAugWISv+sV6pQw8wObwndTrDIEO2A5KQBkH9SG78emJIzDEAhEJaD\ncJXHvFdyJJW2WFu9rPoy5u5iMj8TQAbR5gwbgSNU7q5aSLPmpEHXcfFBAoGAcHVs\n8raaiEFDbatssUcb0NrGemcmdvkkwxs1kl5QbVuetBZrXX91DJQazjIzjvmifoqt\nE3bw78ALJFTEUlYUYMYxhoCTp/QdElfw2YmiqkeuyZcUZsKnP02uWXduUkiHYpdZ\nZbFePBMOMItzYBJ5jIbcVT1YMSDA91p/V9dZQIECgYEA4LmiwVFXxUXqgqupvSGc\nfHGofUagTf3JcKFP76BPreZ63SRss2g8JZNXsj8S3l22hn6ldPZRTZvRi5bWCByT\n9g5obkC2zCuynbKfU03fJiinLodmYDJ7NhVcr3r4uxeaXAcgrPeQUDmU57mli3vT\nOIrvKi1mxhZXLorH3V0RZ30=\n-----END PRIVATE KEY-----\n",
+    "client_email": "firebase-adminsdk-5ryru@tempgigachat.iam.gserviceaccount.com",
+    "client_id": "108294213973911993423",
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
     "token_uri": "https://oauth2.googleapis.com/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-5avio%40gigachat-img.iam.gserviceaccount.com",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-5ryru%40tempgigachat.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
   }),
-  storageBucket: 'gigachat-img.appspot.com',
+  storageBucket: 'tempgigachat.appspot.com',
 });
 
 const storage = admin.storage();
